702543c6db93715e163fae6fc9eefad1cee7064f
Alok Saxena <alok@leewayhertz.com>
2021-08-26 01:36:24 +0530

    repo inital setup

--- app/manager/manager.js
+++ app/manager/manager.js
@@ -3,14 +3,28 @@ var solc = require('solc');
 class BLManager {
     async VerifyContract(data) {
     	let optimise = false
-    	let version = '0.4.24'
+    	let version = 'v0.4.24+commit.e67f0147'
     	var soliCompCache = {};
     	var output = null
     	let res = JSON.parse(data)
+    	var targetSolc = soliCompCache[version];
     	// getting the development snapshot
-		output = JSON.parse(solc.compile(JSON.stringify(res.verifycode)));
+    	solc.loadRemoteVersion(version, function (err, solcV) {
+          console.log("on loadRemoteVersion:" + version);
+          if (err) {
+            console.error(err);
+            
+          }
+          else {
+            targetSolc = solcV;
+            soliCompCache[version] = targetSolc;//compiler cache
+             output = targetSolc.compile(res.verifycode, optimise);
+            
+          }
+        });
+		//output = JSON.parse(solc.compile(JSON.stringify(res.verifycode)));
         
-        console.log('hello===>',res.verifycode)
+        //console.log('hello===>',res.verifycode)
         return output;
     }
 }
