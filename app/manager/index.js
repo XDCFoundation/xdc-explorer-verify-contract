b00a2ed61fe0864cfae1d653b64ac180915cbebc
Alok Saxena <alok@leewayhertz.com>
2021-08-27 13:45:31 +0530

    updated

--- app/manager/index.js
+++ app/manager/index.js
@@ -9,13 +9,13 @@ const { httpConstants, apiSuccessMessage, apiFailureMessage, } = require("../com
 
 class Manger {
     async getVerifyContract(req) {
-        lhtLog("Manager:getVerifyContract", "getVerifyContract started", Config.IS_CONSOLE_LOG, "");
+        //lhtLog("Manager:getVerifyContract", "getVerifyContract started", Config.IS_CONSOLE_LOG, "");
         
         let [error, response] = await parseResponse(
             new BlManager().VerifyContract(req)
         );
         if (error) {
-            lhtLog("Manager:getVerifyContract", "getVerifyContract end", error, "", "ERROR");
+           // lhtLog("Manager:getVerifyContract", "getVerifyContract end", error, "", "ERROR");
             throw structuredResponse(error, error.message || apiFailureMessage.FETCH_FAIL, httpConstants.RESPONSE_STATUS.FAILURE, httpConstants.RESPONSE_CODES.FORBIDDEN);
         }
         return structuredResponse(response, apiSuccessMessage.SEARCH_SUCCESSFULL, httpConstants.RESPONSE_STATUS.SUCCESS, httpConstants.RESPONSE_CODES.OK);
