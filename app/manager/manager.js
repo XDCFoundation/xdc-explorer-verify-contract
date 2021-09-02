ab5f65c1aa7e95f3ba7c482c24a10d3ff88facf1
Alok Saxena <alok@leewayhertz.com>
2021-09-02 17:52:28 +0530

    updated

--- app/manager/manager.js
+++ app/manager/manager.js
@@ -4,8 +4,32 @@ const fs = require('fs');
 const path = require('path');
 class BLManager {
     async VerifyContract(data) {
-    	console.log('params====>',data)
-    	return data;
+    	if(data.action == 'compile'){
+    		let version = data.version
+    		let optimise = (data.optimise > 0)  ? 1 : 0
+    		let address = data.addr
+    		let code = data.code
+    		let name = data.contractname
+    		var concatByteCode = "";
+            var verifiedContracts = [];
+    		if(version == 'latest'){
+
+    		}else{
+    			solc.loadRemoteVersion(version, function (err, solcV) {
+    				var output = solcV.compile(source, optimise);
+    				for (var contractName in output.contracts) {
+		              concatByteCode += output.contracts[contractName].bytecode;
+		              verifiedContracts.push({
+		                "name": contractName,
+		                "abi": output.contracts[contractName].interface,
+		                "bytecode": output.contracts[contractName].bytecode
+		              });
+            		}
+    			});
+    		}
+    	}
+    	console.log('params====>',verifiedContracts)
+    	return verifiedContracts;
     }
 }
 
